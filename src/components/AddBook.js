c0314f803e56452ff70a0d8b939e7210895209b3
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-11 20:30:44 +0500

    Update Read Me File

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -5,6 +5,7 @@ const AddBook = () => (
     <h1>Add New Book</h1>
     <input type="text" placeholder="Enter title of the book" id="title" />
     <input type="text" placeholder="Enter author name" id="author" />
+    <input type="text" placeholder="Enter category" id="category" />
     <button type="button" text="Add" value="Add" label="Add">Add Book</button>
   </div>
 );
