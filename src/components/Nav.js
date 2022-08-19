5fb5392bc22b6400f02e60f102a26f1e9df73e9c
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-08-19 17:47:17 +0500

    Adding some styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import title from '../images/title.png';
+import login from '../images/login.png';
 
 const Nav = () => (
-  <div className="nav">
-    <h1>Bookstore CMS</h1>
-    <nav>
-      <ul>
-        <Link to="/"><li>Books</li></Link>
-        <Link to="/categories"><li>Categories</li></Link>
-      </ul>
-    </nav>
+  <div className="header">
+    <div className="nav">
+      <img src={title} alt="title comes here" />
+      <nav className="navigation">
+        <ul>
+          <Link to="/"><li>Books</li></Link>
+          <Link to="/categories"><li>Categories</li></Link>
+        </ul>
+      </nav>
+    </div>
+    <div className="login">
+      <img src={login} alt="login comes here" />
+    </div>
   </div>
 );
 
